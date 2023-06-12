bcf44e366b4223b97ba5dc56ad1ac5fc854af351
arc4t <arthur.hauswald@gmail.com>
2023-06-12 16:00:22 +0200

    refactor:  put printer ip inside local variable

--- api_call.py
+++ api_call.py
@@ -1,8 +1,10 @@
 import requests
 
+PRINTER_IP = "192.168.0.120"
+
 def get_printer_status():
     '''This function will return the status of the printer'''   
-    url = "http://192.168.0.120/api/v1/printer"
+    url = "http://"+PRINTER_IP+"/api/v1/printer"
     header = {"Accept": "application/json"}
 
     printer_status_request = requests.get(url, headers=header)
@@ -11,7 +13,7 @@ def get_printer_status():
 
 def get_current_time():
     '''This function will return the current job of the printer'''
-    url = "http://192.168.0.120/api/v1/system/time"
+    url = "http://"+PRINTER_IP+"/api/v1/system/time"
     header = {"Accept": "application/json"}
     
     printer_time_request = requests.get(url, headers=header)
@@ -20,7 +22,7 @@ def get_current_time():
 
 def get_printing_time_total():
     '''This function will return the estimated time to complete the print job'''
-    url = "http://192.168.0.120/api/v1/print_job/time_total"
+    url = "http://"+PRINTER_IP+"/api/v1/print_job/time_total"
     header = {"Accept": "application/json"}
     
     printer_time_total_request = requests.get(url, headers=header)
@@ -29,7 +31,7 @@ def get_printing_time_total():
 
 def get_printing_time_elapsed():
     '''This function will return the elapsed time of the print job'''
-    url = "http://192.168.0.120/api/v1/print_job/time_elapsed"
+    url = "http://"+PRINTER_IP+"/api/v1/print_job/time_elapsed"
     header = {"Accept": "application/json"}
     
     printer_time_elapsed_request = requests.get(url, headers=header)
@@ -38,7 +40,7 @@ def get_printing_time_elapsed():
 
 def get_printing_progress():
     '''This function will return the progress of the print job'''
-    url = "http://192.168.0.120/api/v1/print_job/progress"
+    url = "http://"+PRINTER_IP+"/api/v1/print_job/progress"
     header = {"Accept": "application/json"}
     
     printer_progress_request = requests.get(url, headers=header)
